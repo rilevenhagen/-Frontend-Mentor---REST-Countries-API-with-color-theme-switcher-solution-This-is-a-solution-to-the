51d47cc1f23fa0eaf35106c93484203cc37e1f86
Ricardo <rlevenhagen@gmail.com>
2022-05-25 14:52:22 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+import { useEffect, useState, useRef } from 'react';
 import Header from './Header';
 import './App.css';
 import './component/Api'
--- src/component/Api.js
+++ src/component/Api.js
@@ -1,8 +1,31 @@
-
+import axios from 'axios';
+import { useEffect, useState, useRef } from 'react';
 //get data from api
 
-const url = `https://restcountries.com/v2/name/{name}`;
+
+function Api() {
+
+const [flag, setFlag] = useState([]);
+// const [userImput, setuserImput] = useState([]);
+// const [searchFlag, searchFlag] = useState([]);
+
+
+const url = `https://restcountries.com/v2/all`;
+
+axios({
+    method: 'GET',
+    url: url,
+    }.then((response) =>{
+        return console.log(response);
+        // setFlag(response.data);
+    }).catch(function (error){
+        console.log(error)
+    })
+)
 
 console.log(url);
 
-// export default Api
+}
+
+
+export default Api
